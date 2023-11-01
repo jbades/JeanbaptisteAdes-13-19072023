e1628c36c4e1374f53fd742e07727f92f5f79c71
jbades <jbadespro@gmail.com>
2023-11-01 16:41:39 +0100

    setting redux-persist

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import "./styles/sass/_main.scss";
-import { Provider } from "react-redux";
-import store from "./store"
-import App from './App';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import "./styles/sass/_main.scss"
+import { Provider } from "react-redux"
+import { store, persistor } from "./store"
+import App from './App'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,34 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userProfileReducer from './features/userProfile'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+const persistConfig = {
+    key: 'root',
+    storage,
+  };
+
+const persistedReducer = persistReducer(persistConfig, userProfileReducer)
+
+const actionLoggerMiddleware = store => next => action => {
+    console.log('Dispatching action:', action);
+    const result = next(action);
+    console.log('New state:', store.getState());
+    return result;
+};
 
 const store = configureStore ({
     reducer: {
-        userProfile: userProfileReducer
-    }
+        userProfile: persistedReducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(actionLoggerMiddleware)
 })
+
+const persistor = persistStore(store);
 
 // state tracker
 store.subscribe(() => {
     console.log("!!! New state: ", store.getState())
 })
 
-export default store
+export {store, persistor}
